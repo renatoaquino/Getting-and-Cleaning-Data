c13104653eb6a0af9623b48c5000f6bc2f8fbbdc
Renato Aquino <renato.aquino@gmail.com>
2015-04-23 16:04:08 -0300

    Adding description and working code

---
+++ README.md
@@ -0,0 +1,16 @@
+# Getting and Cleaning Data Course Project
+
+The purpose of this project is to prepare a tidy data that can be used for later analysis.
+The file run_analysis.R can be run as long as the Samsung data is in your working directory (Folder "UCI HAR Dataset").
+
+After executing the run_analysis.R file, a dataset.txt file will be generated with a tidy dataset from the Samsung data.
+
+The generated database can be easily read with read.table:
+```
+data <- read.table("dataset.txt", header = TRUE)
+```
+
+Database Source:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+Details about the generated data can be found at the file Codebook.md
--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,3 @@
-library(stringr)
-
 # This function reads and merges the test and train datasets
 # The file locations are hardcoded to getwd()+"/UCI HAR Dataset/*"
 mergeAndLabelDatasets <- function(){  
@@ -8,10 +6,10 @@ mergeAndLabelDatasets <- function(){
   activityLabels <- t(read.csv("UCI HAR Dataset/activity_labels.txt",sep=" ",colClasses=c("NULL","character"),header=FALSE))
   
   #Performs some transformations for easy usage later
-  interestingColumns <- str_detect(columnLabels,"-mean()") | str_detect(columnLabels,"-std()")
+  interestingColumns <- grepl("mean",columnLabels) | grepl("std",columnLabels)
   colClasses <- rep("NULL",ncol(columnLabels))
   colClasses[interestingColumns] <- "numeric"
-  colNames <- columnLabels[interestingColumns]
+  colNames <- gsub("\\(|\\)",'',tolower(columnLabels[interestingColumns]))
   colWidths <- rep(16,ncol(columnLabels))
   
   #Clean as much as we can for now
@@ -19,36 +17,44 @@ mergeAndLabelDatasets <- function(){
   rm(interestingColumns)
   
   #Reads the train dataset, associate the subjects, activity labels and labels the columns
-  train <- read.fwf("UCI HAR Dataset/train/X_train.txt",widths=colWidths,colClasses=colClasses,n=10)
+  train <- read.table("UCI HAR Dataset/train/X_train.txt",colClasses=colClasses)
+  #train <- laf_open_fwf("UCI HAR Dataset/train/X_train.txt",column_widths=colWidths,column_types=colClasses)
   names(train) <- colNames
-  train$Subject <- read.fwf("UCI HAR Dataset/train/subject_train.txt",widths=c(2),colClasses=c("numeric"),n=10)[1,]
-  trainActivities <- t(read.table("UCI HAR Dataset/train/y_train.txt",header=FALSE,nrows=10))
-  train$Activity <- activityLabels[trainActivities]
+  train$subject <- scan("UCI HAR Dataset/train/subject_train.txt")
+  trainActivities <- scan("UCI HAR Dataset/train/y_train.txt")
+  train$activity <- trainActivities
   rm(trainActivities)
-  train$Dataset <- 'train'
   
   #Reads only what is needed from the test dataset
-  test <- read.fwf("UCI HAR Dataset/test/X_test.txt",widths=colWidths,colClasses=colClasses,n=10)
+  test <- read.table("UCI HAR Dataset/test/X_test.txt",colClasses=colClasses)
   #Name the columns
   names(test) <- colNames
   #Attach the subject of the activity
-  test$Subject <- read.fwf("UCI HAR Dataset/test/subject_test.txt",widths=c(2),colClasses=c("numeric"),n=10)[1,]
-  #Attach the activity names
-  testActivities <- t(read.table("UCI HAR Dataset/test/y_test.txt",header=FALSE,nrows=10))
-  test$Activity <- activityLabels[testActivities]
-  test$Dataset <- 'test'
+  test$subject <- scan("UCI HAR Dataset/test/subject_test.txt")
+  #Attach the activities
+  testActivities <- scan("UCI HAR Dataset/test/y_test.txt")
+  test$activity <- testActivities
   
   #More cleanup before the big merge
   rm(testActivities)
-  rm(activityLabels)
   rm(colClasses)
   rm(colNames)
+  dataset <- merge(train,test,all=TRUE,sort=FALSE)
+  rm(train)
+  rm(test)
   
-  result <- merge(train,test,all=TRUE,sort=FALSE)
-  return(result)
+  #Aggregating the data by Subject and Activity
+  attach(dataset)
+  aggr <- aggregate(dataset,by=list(GSubject=subject,GActivity=activity),FUN=mean)
+  detach(dataset)
+  aggr$activityLabel <- activityLabels[aggr$activity]
+  return(subset(aggr, select = -c(GSubject,GActivity) ))
 }
 
-#Given a treated dataset, calculates the average by subject and activity
-avgDataSet <- function(dataset){
-  
-}
+oldwd <- getwd()
+setwd(dirname(parent.frame(2)$ofile))
+dataset = mergeAndLabelDatasets()
+write.table(dataset,file='dataset.txt',row.names=FALSE)
+rm(dataset)
+setwd(oldwd)
+rm(oldwd)
